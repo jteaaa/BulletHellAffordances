f4aec8015e1486cf75a9126fdb641aea73ff1413
browndiego <diegobrown@ucsb.edu>
2022-06-23 10:48:47 -0700

    turrets

---
+++ Bullet Hell Affordances/Assets/Scripts/BasicTurret.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTurret : MonoBehaviour
+{
+
+    public float interval = 3;
+    public BasicBullet bullet;
+    private bool waiting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waiting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!waiting)
+        {
+            Object.Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), new Quaternion(1, 0, 0, 0));
+            waiting = true;
+            StartCoroutine(waiter(interval));
+        }
+    }
+
+    IEnumerator waiter(float inter)
+    {
+        yield return new WaitForSeconds(inter);
+        waiting = false;
+    }
+}
